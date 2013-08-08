2bf841c8e306f9e461a20796775e6a5e6fddd0db
Angel <angel.arancibia@gmail.com>
2013-08-08 08:58:00 -0300

    Remove assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -54,9 +54,9 @@ module TpFinal
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.2'
   end
 end
