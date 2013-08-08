7b9b06ebe113eca9c5e8e5e746070461e8b094f2
Angel <angel.arancibia@gmail.com>
2013-08-08 11:16:52 -0300

    Requiere user by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
+
   layout :layout_by_resource
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,10 @@ TpFinal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  authenticated :user do
+    root :to => 'home#index', :as => :authenticated_root
+  end
+  root :to => redirect('/users/sign_in')
 
   # See how all your routes lay out with "rake routes"
 
